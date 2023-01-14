a4f1a5fbe75d84b4bba04a99c8b647433c729886
Will Galebach <willbach@gmail.com>
2023-01-14 17:35:50 +0000

    Switching to CRA

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { App } from './app';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import './index.css';
-import { Buffer as BufferPolyfill } from 'Buffer'
-declare var Buffer: typeof BufferPolyfill;
-// @ts-ignore
-globalThis.Buffer = BufferPolyfill
-
-// import { Buffer } from "buffer/";
-// window.Buffer = Buffer;
 
 ReactDOM.render(
   <React.StrictMode>
